0cd645b2f0092a5892245771bbb0e2a51803612d
OKAN TOPER <okan.toper94@gmail.com>
2023-01-27 16:57:26 +0300

    packages added

--- routes/shop.js
+++ routes/shop.js
@@ -4,9 +4,13 @@ const express = require('express');
 
 const rootDir = require('../util/path');
 
+const adminData = require('./admin');
+
 const router = express.Router();
 
+
 router.get('/', (req, res, next) => {
+  console.log('shop',adminData.products)
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
