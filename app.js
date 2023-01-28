9edeb1bfe964405cf73b1d45ca4a992ae82ef6fe
OKAN TOPER <okan.toper94@gmail.com>
2023-01-28 12:35:47 +0300

    edit

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 
-const expressHbs = require('express-handlebars')
+//const expressHbs = require('express-handlebars') //for hbs
 
 const app = express();
 
 
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set('view engine', 'hbs')
+//app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})); //for hbs
+//app.set('view engine', 'hbs')
+app.set('view engine', 'ejs')
 //app.set('view engine', 'pug')
 app.set('views', 'views')
 
