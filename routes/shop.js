27b1073de3aa508e3a57ae2b990f686f2c364f93
OKAN TOPER <okan.toper94@gmail.com>
2023-01-28 12:12:07 +0300

    handlebars completed

--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,15 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/shop'})
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/shop',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    formsCSS:true,
+    productCSS: true,
+  })
 });
 
 module.exports = router;
