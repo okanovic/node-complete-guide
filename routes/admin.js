ff04c289a4d57a407f72d162a912e950fd696ceb
OKAN TOPER <okan.toper94@gmail.com>
2023-02-04 13:12:12 +0300

    controller fixed

--- routes/admin.js
+++ routes/admin.js
@@ -10,9 +10,9 @@ const router = express.Router();
 router.get('/add-product', adminController.getAddProduct);
 
 // /admin/products => GET
-router.get('/products');
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', adminController.postAddProduct );
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
