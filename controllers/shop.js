4f8433e0ca49d1d215dc82446e52d5e742c18793
OKAN TOPER <okan.toper94@gmail.com>
2023-02-05 20:20:35 +0300

    cart ejs entegrated

--- controllers/shop.js
+++ controllers/shop.js
@@ -32,9 +32,21 @@ exports.getIndex = (req, res, next) => {
 }
 
 exports.getCart = (req, res, nex) => {
-    res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart'
+    Cart.getCart(cart => {
+        Product.fetchAll(products => {
+            const cartProducts = [];
+            for (const product of products) {
+                const cartProductData = cart.products.find(prod => prod.id.trim() === product.id.trim());
+                if (cartProductData) {
+                    cartProducts.push({ productData: product, qty: cartProductData.qty })
+                }
+            }
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                products: cartProducts
+            })
+        })
     })
 }
 
--- models/cart.js
+++ models/cart.js
@@ -47,7 +47,17 @@ module.exports = class Cart {
                 console.log(err)
             })
         })
+    }
 
+    static getCart(cb) {
+        fs.readFile(p, (err, fileContent) => {
+            const cart = JSON.parse(fileContent);
+            if (err) {
+                cb(null)
+            }else{
+                cb(cart)
+            }
+        })
     }
 
 
