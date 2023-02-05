7c86afb5c9ef1907c8bd825dd41ff70c401828a0
OKAN TOPER <okan.toper94@gmail.com>
2023-02-05 12:47:33 +0300

    product detail completed

--- routes/shop.js
+++ routes/shop.js
@@ -9,6 +9,9 @@ const router = express.Router();
 
 router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
+
+//products/.....
+router.get('/products/:productId', shopController.getProduct)
 router.get('/cart', shopController.getCart);
 router.get('/orders', shopController.getOrders);
 router.get('/checkout', shopController.getCheckout);
